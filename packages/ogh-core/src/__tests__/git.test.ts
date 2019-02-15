1a8155afd444841549b799d78a788594334b9125
Yuki Yamada <yamadayuuki0818@gmail.com>
2019-02-15 13:32:17 +0900

    Remove unused code

--- packages/ogh-core/src/__tests__/git.test.ts
+++ packages/ogh-core/src/__tests__/git.test.ts
@@ -1,6 +1,6 @@
 import * as child_process from "child_process";
 import * as mock from "mock-fs";
-import { getDotGitDirectory, getGitHooksDirectory, getRootDirectory, getStagedFiles, getUnstagedFiles } from "../git";
+import { getGitHooksDirectory } from "../git";
 
 // Mocks
 jest.mock("child_process");
@@ -47,20 +47,6 @@ const mockGit = () => {
 
 // Tests
 describe("git module", () => {
-  describe("getDotGitDirectory", () => {
-    it("calls `git rev-parse --git-dir`", () => {
-      mockGit();
-      getDotGitDirectory();
-
-      expect(child_process.execSync).toHaveBeenCalledWith("git rev-parse --git-dir");
-    });
-
-    it("returns the full path instead of relative path", () => {
-      mockGit();
-      expect(getDotGitDirectory()).toBe("/.git");
-    });
-  });
-
   describe("getGitHooksDirectory", () => {
     it("calls `git rev-parse --git-path hooks`", () => {
       mockGit();
@@ -72,52 +58,6 @@ describe("git module", () => {
     it("returns the full path instead of relative path and concatenaited with argument", () => {
       mockGit();
       expect(getGitHooksDirectory()).toBe("/.git/hooks");
-    });
-  });
-
-  describe("getRootDirectory", () => {
-    it("calls execSync once", () => {
-      mockGit();
-      getRootDirectory();
-
-      expect(child_process.execSync).toHaveBeenCalledTimes(1);
-    });
-
-    it("returns the root directory", () => {
-      mockGit();
-      expect(getRootDirectory()).toBe("/");
-    });
-  });
-
-  describe("getStagedFiles", () => {
-    it("calls `git diff --cached --name-only`", () => {
-      mockGit();
-      getStagedFiles();
-
-      expect(child_process.execSync).toHaveBeenLastCalledWith("git diff --cached --name-only", { cwd: "/" });
-    });
-
-    it("returns only staged files", () => {
-      mockGit();
-      const staged = getStagedFiles();
-      expect(staged).toHaveLength(1);
-      expect(staged).toEqual(["./foo.js"]);
-    });
-  });
-
-  describe("getUnstagedFiles", () => {
-    it("calls `git diff --name-only`", () => {
-      mockGit();
-      getUnstagedFiles();
-
-      expect(child_process.execSync).toHaveBeenLastCalledWith("git diff --name-only", { cwd: "/" });
-    });
-
-    it("returns only unstaged files", () => {
-      mockGit();
-      const unstaged = getUnstagedFiles();
-      expect(unstaged).toHaveLength(2);
-      expect(unstaged).toEqual(["./bar.md", "./baz.js"]);
     });
   });
 });
--- packages/ogh-core/src/git.ts
+++ packages/ogh-core/src/git.ts
@@ -1,42 +1,10 @@
 import { execSync } from "child_process";
 import { resolve } from "path";
 
-export function getDotGitDirectory(): string {
-  return resolve(
-    execSync("git rev-parse --git-dir")
-      .toString()
-      .trimRight()
-  );
-}
-
 export function getGitHooksDirectory(): string {
   return resolve(
     execSync("git rev-parse --git-path hooks")
       .toString()
       .trimRight()
   );
-}
-
-export function getRootDirectory(): string {
-  return resolve(getDotGitDirectory(), "..");
-}
-
-export function getStagedFiles(): string[] {
-  return execSync("git diff --cached --name-only", { cwd: getRootDirectory() })
-    .toString()
-    .split("\n")
-    .filter(s => s.length > 0);
-}
-
-export function getUnstagedFiles(): string[] {
-  return execSync("git diff --name-only", { cwd: getRootDirectory() })
-    .toString()
-    .split("\n")
-    .filter(s => s.length > 0);
-}
-
-export function stageFiles(files: string[]) {
-  const filesStr = files.join(" ");
-
-  execSync(`git add ${filesStr}`, { cwd: getRootDirectory() });
 }
